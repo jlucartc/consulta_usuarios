ad31e4abc70e7ca373d205359709a00a81c89301
jlucartc <jlucartc@gmail.com>
2022-03-29 18:09:29 -0300

    Removo Tempfiles do model de Imagem e volto a salvar os arquivos após inserção da linha no banco

--- app/models/imagem.rb
+++ app/models/imagem.rb
@@ -2,16 +2,20 @@ class Imagem < ApplicationRecord
 	belongs_to :usuario
 	validates :nome, uniqueness: :true
 	before_create :escolhe_nome
+	after_create :cria_arquivo
 
 	def escolhe_nome
 		self.nome = "#{self.nome.split('.').first}_#{SecureRandom.hex(20)}_.#{self.nome.split('.').last}"
 		escolhe_nome if Imagem.where(nome: self.nome).any?
 	end
 
-	def path
-		tempfile = Tempfile.new(self.nome,"public/images/",binmode: true)
-		tempfile.write(self.arquivo)
-		tempfile.path.gsub(/.*public/,'')
+	def cria_arquivo
+		arquivo = File.open("public/images/#{self.nome}",'wb')
+		arquivo.write(self.arquivo)
+		arquivo.close
 	end
 
+	def path
+		"/images/#{self.nome}"
+	end
 end
