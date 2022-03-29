9649762343b25f7dbf38deba9c00ffbf9cde01e8
jlucartc <jlucartc@gmail.com>
2022-03-28 22:34:08 -0300

    Adicionando métodos de index e busca ao controller da aplicação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+	def index
+		@usuarios = Usuario.all
+	end
+
+	def buscar
+		params[:busca] = params[:busca].upcase.gsub(/\s+/,'.*')
+		@usuarios = Usuario.all.filter{|usuario| I18n.transliterate(usuario.nome.upcase).match?(Regexp.new(".*#{params[:busca]}.*"))}
+		@usuarios = Usuario.where(id: @usuarios.pluck(:id)).page(params[:current_page]).per(10)
+		@total_pages = @usuarios.total_pages
+		@total_resultados = @usuarios.total_count
+		@page = params[:current_page].to_i
+	end
 end
