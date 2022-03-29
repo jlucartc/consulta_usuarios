e5c93014638121c8e9f5711fb3c391fbc89e9313
jlucartc <jlucartc@gmail.com>
2022-03-28 22:35:03 -0300

    Adicionando validações, relações e métodos em módels de Usuario e Imagem

--- app/models/imagem.rb
+++ app/models/imagem.rb
@@ -1,2 +1,22 @@
 class Imagem < ApplicationRecord
+	belongs_to :usuario
+	validates :nome, uniqueness: :true
+	before_create :escolhe_nome
+	after_create :cria_arquivo
+
+	def escolhe_nome
+		self.nome = "#{self.nome.split('.').first}_#{SecureRandom.hex(20)}_#{self.nome.split('.').last}"
+		escolhe_nome if Imagem.where(nome: self.nome).any?
+	end
+
+	def cria_arquivo
+		arquivo = File.open("public/images/#{self.nome}",'wb')
+		arquivo.write(self.arquivo)
+		arquivo.close
+	end
+
+	def path
+		"images/#{self.nome}"
+	end
+
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,15 @@
 class Usuario < ApplicationRecord
+	has_many :imagens
+
+	def thumb_image
+		self.imagens.filter{|img| img.nome.match?(/.*_thumb_.*/) }.first
+	end
+
+	def medium_image
+		self.imagens.filter{|img| img.nome.match?(/.*_thumb_.*/) }.first
+	end
+
+	def large_image
+		self.imagens.filter{|img| img.nome.match?(/.*_thumb_.*/) }.first
+	end
 end
