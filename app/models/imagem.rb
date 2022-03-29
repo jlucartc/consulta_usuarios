866854de54e858c5e20a116700ff3fba7edb7e76
jlucartc <jlucartc@gmail.com>
2022-03-29 15:21:06 -0300

    Salvando imagens em Tempfiles para tentar apresentar imagens na view

--- app/models/imagem.rb
+++ app/models/imagem.rb
@@ -2,21 +2,16 @@ class Imagem < ApplicationRecord
 	belongs_to :usuario
 	validates :nome, uniqueness: :true
 	before_create :escolhe_nome
-	after_create :cria_arquivo
 
 	def escolhe_nome
 		self.nome = "#{self.nome.split('.').first}_#{SecureRandom.hex(20)}_.#{self.nome.split('.').last}"
 		escolhe_nome if Imagem.where(nome: self.nome).any?
 	end
 
-	def cria_arquivo
-		arquivo = File.open("public/images/#{self.nome}",'wb')
-		arquivo.write(self.arquivo)
-		arquivo.close
-	end
-
 	def path
-		"/images/#{self.nome}"
+		tempfile = Tempfile.new(self.nome)
+		tempfile.write(self.arquivo)
+		tempfile.path
 	end
 
 end
