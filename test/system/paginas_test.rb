dabb38fd9b2aa8eb9b1446da8cc5a9f063c7d99f
jlucartc <jlucartc@gmail.com>
2022-03-30 09:55:13 -0300

    Adiciono teste para verificar se os usuários na view condizem com o que foi obtido na consulta paginada

---
+++ test/system/paginas_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class PaginasTest < ApplicationSystemTestCase
+  test "View deve apresentar o mesmo número de resultados contidos na pagina atual da consulta" do
+    visit "#{buscar_path}?current_page=1&busca=mr"
+    resultados = all('.resultado-card')
+    assert resultados.count == busca_resultados_pagina('mr',1).count
+  end
+end
