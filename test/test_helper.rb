9b09f861f041534c8a9dccef422b480b1490087d
jlucartc <jlucartc@gmail.com>
2022-03-30 09:52:28 -0300

    Adiciono helper para testes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,12 @@
+require 'simplecov'
+SimpleCov.start
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "helper"
 
 class ActiveSupport::TestCase
+  include Helper
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
