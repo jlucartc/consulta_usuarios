86d5928e67a76aa31fa02a8d375ed3e11087bcea
jlucartc <jlucartc@gmail.com>
2022-03-30 09:42:50 -0300

    Adiciono teste para exclusão de usuário e suas imagens

--- test/models/imagem_test.rb
+++ test/models/imagem_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ImagemTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+
+  setup do
+    cria_imagens_teste
+  end
+
+  teardown do
+    deleta_imagens_teste
+  end
+
+  test "deve destruir as imagens caso o usuário seja destruido" do
+    usuario = usuarios(:one)
+    assert usuario.destroy
+    assert usuario.imagens.count == 0
   end
 end
